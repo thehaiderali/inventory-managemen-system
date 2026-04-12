294099b01341037f97e032f3f676c2eb036221f4
noyansiddiqui99-maker <noyansiddiqui99@gmail.com>
2026-04-13 00:45:55 +0500

    Configure dotenv and path in seed.js
    Added path and dotenv configuration for environment variables and inserted new columns in suppliers and warehouses.

--- backend/Database/seed.js
+++ backend/Database/seed.js
@@ -5,7 +5,9 @@ import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = path.dirname(__filename);
+
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const dbConfig = {
@@ -20,10 +22,8 @@ const dbConfig = {
   },
 };
 
-
-async function seed() {
+async function seed(){
   let pool;
-
   try {
     console.log("Connecting to database...");
     pool = await sql.connect(dbConfig);
